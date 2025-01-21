0f7c6af0958acfc7bdeedf0487589b23286c1ca5
JovannyRch <jovannyrch@gmail.com>
2025-01-21 09:10:46 -0600

    Add build

--- public/build/assets/Welcome-CeZohsis.js
+++
@@ -1,1 +0,0 @@
-import{j as e,L as o}from"./app-BD-75fSJ.js";function n({auth:r,laravelVersion:t,phpVersion:s}){return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"Welcome"}),"Hola mundo :D"]})}export{n as default};
